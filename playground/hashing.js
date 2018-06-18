3aa1f16ac18ae5dfe00727f19d8cc0e6673df0fe
tatarksb <tatarksb@windowslive.com>
2018-06-18 13:24:06 +0700

    Add POST /users/ route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,38 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+  id: 10
+};
+
+var token = jwt.sign(data, '123abc');
+console.log(token);
+
+var decoded = jwt.verify(token, '123abc');
+console.log('decoded: ', decoded);
+
+// var massage = 'I am user number 3';
+// var hash = SHA256(massage).toString();
+//
+// console.log(`Message: ${massage}`);
+// console.log(`Hash: ${hash}`);
+//
+// var data= {
+//   id: 4
+// };
+//
+// var token = {
+//   data,
+//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+// };
+//
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
+//
+// var resultHash = SHA256(JSON.stringify(token.data)+ 'somesecret').toString();
+//
+// if (resultHash === token.hash) {
+//   console.log('Data was not changed');
+// } else {
+//   console.log('Data was changed. Do not trust');
+// }
