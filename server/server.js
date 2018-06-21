cab5a5be59efbf078969eb65e49f3a2c3648cb92
tatarksb <tatarksb@windowslive.com>
2018-06-21 12:49:56 +0700

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -93,6 +93,7 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
+// POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
@@ -108,6 +109,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate,  (req, res) => {
   res.send(req.user);
+})
+
+// POST /users/login
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 })
 
 app.listen(port, () => {
